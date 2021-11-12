d74651e1dddb440208830d0bc4d0a5c42380b587
SamSim <jun78111209@gmail.com>
2021-11-12 16:10:16 +0900

    db migrate완료, admin page 계정 등록

--- ssulocker/locker/admin.py
+++ ssulocker/locker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import personalinfo
+from .models import locker
+admin.site.register(personalinfo)
+admin.site.register(locker)
 # Register your models here.
--- ssulocker/ssulocker/settings.py
+++ ssulocker/ssulocker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'locker.apps.LockerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
