2854c63d39449c346cdd37571e9ecf9ce46292ba
SamSim <jun78111209@gmail.com>
2021-11-15 20:49:54 +0900

    admin
    admin question 추가

--- ssulocker/locker/admin.py
+++ ssulocker/locker/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import personalinfo
 from .models import locker
-admin.site.register(personalinfo)
 admin.site.register(locker)
+class userAdmin(admin.ModelAdmin):
+    search_fields=['user']
+admin.site.register(personalinfo,userAdmin)
 # Register your models here.
