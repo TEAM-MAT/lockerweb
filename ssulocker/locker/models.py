5dc30c24fd6dd2f46c9ec90fc1efdf2ce90cdc0e
SamSim <jun78111209@gmail.com>
2021-11-15 14:03:38 +0900

    model수정,생성실습

--- ssulocker/locker/models.py
+++ ssulocker/locker/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 from django.db.models.enums import Choices
 from django.db.models.fields import NullBooleanField
 from django.db.models.fields.related import ForeignKey
+class locker(models.Model):
+    lockernum=models.IntegerField(primary_key=True)
+    floor=models.IntegerField()
+    reserved=models.IntegerField(default=0)#reserved -> 1 unreserved ->0,trigger로 바꿔줘야할듯
+    sector=models.CharField(max_length=2,help_text='층별 구역',null=True)
+# Create your models here.
 class personalinfo(models.Model):
     name=models.CharField(max_length=10,help_text='이름',null=False)
     id=models.CharField(max_length=8,help_text="학번",primary_key=True,null=False)
     pw=models.CharField(max_length=20,null=False)
-    lockernum=models.IntegerField()
+    lockernum=models.ForeignKey(locker,related_name="lockerusing",on_delete=SET_NULL,db_column="lockernum",null=True)
     departments=(('cs','컴퓨터학부'),
     ('GM','글로벌미디어학부'),('EIE','전자정보공학부'),('SW','소프트웨어학부'),
     ('AIC','AI융합학부'))
     department=models.CharField(max_length=4,choices=departments)
-class locker(models.Model):
-    lockernum=models.IntegerField(primary_key=True)
-    floor=models.IntegerField()
-    reserved=models.ForeignKey("personalinfo",related_name="reservedstudent",on_delete=models.SET_NULL,db_column="reserved",null=True)
-    sector=models.CharField(max_length=2,help_text='층별 구역',null=True)
-# Create your models here.
+
