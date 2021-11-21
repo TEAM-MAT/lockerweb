2c9e87cab9a14d3cb7e09f32ac7197aa48b467fb
SamSim <jun78111209@gmail.com>
2021-11-21 19:24:24 +0900

    view 적용 및 url작성

--- ssulocker/locker/urls.py
+++ ssulocker/locker/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lockerlist',views.lockerlist,name='lockerlist')
 ]
--- ssulocker/locker/views.py
+++ ssulocker/locker/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 from .models import locker
 def index(request):
+    return render(request,'locker/index.html')
+def lockerlist(request):
     locker_list=locker.objects.order_by('lockernum')
     context={"locker_list":locker_list}
     return render(request,'locker/lockerlist.html',context)
