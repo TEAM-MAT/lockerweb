d348ab135a5d2a31f7777bc8558dc6e51361ef67
SamSim <jun78111209@gmail.com>
2021-11-22 14:34:08 +0900

    login추가

--- ssulocker/locker/urls.py
+++ ssulocker/locker/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lockerlist',views.lockerlist,name='lockerlist')
+    path('lockerlist',views.lockerlist,name='lockerlist'),
+    path('login/',auth_views.LoginView.as_view(),name='login')
 ]
--- ssulocker/locker/views.py
+++ ssulocker/locker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 #from django.http import HttpResponse
 from .models import lockers
 def index(request):
