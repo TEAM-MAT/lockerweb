de3a8f61a5f9084f9f51fb2e7ac3221d70d2946b
SamSim <jun78111209@gmail.com>
2021-11-22 16:33:53 +0900

    로그인 기능 삽입 중 오류발생

--- ssulocker/locker/urls.py
+++ ssulocker/locker/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 from django.contrib.auth import views as auth_views
+app_name='locker'
 urlpatterns = [
     path('', views.index, name='index'),
     path('lockerlist',views.lockerlist,name='lockerlist'),
-    path('login/',auth_views.LoginView.as_view(),name='login')
+    path('login/',auth_views.LoginView.as_view(template_name='locker/index.html'),name='login')
 ]
