0126cc83b21d9a673624255e21c1674e3866b948
SamSim <jun78111209@gmail.com>
2021-11-26 14:27:32 +0900

    custom user

--- ssulocker/locker/views.py
+++ ssulocker/locker/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render,get_object_or_404
+import django.contrib.auth as aut
 #from django.http import HttpResponse
 from .models import lockers
 def index(request):
@@ -7,4 +9,10 @@ def lockerlist(request):
     locker_list=lockers.objects.order_by('lockernum')
     context={"locker_list":locker_list}
     return render(request,'locker/lockerlist.html',context)
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=aut.authenticate(request,username=username,passord=password)
+        
 # Create your views here.
