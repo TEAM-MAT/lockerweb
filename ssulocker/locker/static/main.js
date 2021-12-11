88faa8bb968e399d52728147c6df1c3fbc00be86
UnbroKen <unbroken2650@gmail.com>
2021-12-11 14:24:11 +0000

    사물함 5줄 정렬 + 드롭박스 생성

--- ssulocker/locker/static/main.js
+++ ssulocker/locker/static/main.js
@@ -1,5 +1,10 @@
-function popupClose(id) {
-  const target = document.querySelector(`#${id}`);
-  console.log(id);
+function popupClose(inp) {
+  const target = document.querySelector(`#${inp}`);
   target.classList.toggle("hidden");
+}
+
+function selectBuilding(inp) {
+  const building = inp[0] + inp[1];
+  const sector = inp[2];
+  console.log(building, sector);
 }
