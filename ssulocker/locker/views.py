0034252fff19019a1006c2962b4e91d2bc46f5ed
SamSim <jun78111209@gmail.com>
2021-12-13 00:20:53 +0900

    template에 넘어가지를 않음 context가

--- ssulocker/locker/views.py
+++ ssulocker/locker/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render,get_object_or_404
 import django.contrib.auth as aut
 #from django.http import HttpResponse
 from .models import lockers
+import logging
 from django.db import connection
 def index(request):
     #학부 별 남은라커 수/라커 수 구해서 띄워줄 것임.
-    CS_left=lockers.objects.filter(department="CS",reserved=0).count()
+    CS_left=lockers.objects.filter(department="CS",resreved=0).count()
     CS_lockers=lockers.objects.filter(department="CS").count()
-    context={"CS_left":CS_left,"CS_lockers":CS_lockers}
-    return render(request,'locker/index.html',context)
+    locker_context={"cs_left":CS_left,"cs_lockers":CS_lockers}
+    return render(request,'locker/index.html',locker_context)
 def lockerlist(request):
     locker_list=lockers.objects.order_by('lockernum')
     context={"locker_list":locker_list}
