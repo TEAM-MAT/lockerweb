500a59f76857b0eb7f68074e72a67ef1cf80b12f
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-12-14 23:43:51 +0900

    Update resrved_modifier trigger.sql

--- SQL/resrved_modifier trigger.sql
+++ SQL/resrved_modifier trigger.sql
@@ -2,7 +2,10 @@ Delimiter $$
 CREATE trigger reserved_update after update on locker_users
 for each row
 begin
-	UPDATE locker_lockers set locker_lockers.reserved=1 where locker_lockers.lockernum=NEW.lockernum;
+	IF (OLD.lockernum!=null)
+	then UPDATE locker_lockers set locker_lockers.reserved=1 where locker_lockers.lockernum=NEW.lockernum;
     UPDATE locker_lockers set locker_lockers.reserved=0 where locker_lockers.lockernum=OLD.lockernum;
+    ELSE update locker_lockers set locker_lockers.reserved=1 where locker_lockers.lockernum=NEW.lockernum;
+    end if;
 end$$
 Delimiter ;
