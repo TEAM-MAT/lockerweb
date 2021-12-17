63f000d680c2c040623d1d46a0a076ec7fb2f1af
SamSim <jun78111209@gmail.com>
2021-12-18 04:05:35 +0900

    메인화면 남은자리 안뜨던거 해결
    로그인을 지원하도록 django.contrib.auth의 loginview as view를 사용했는데 이것 때문에  views.py의 index함수가 실행이 안되고 있었음. 그래서 index함수를 실행시키기 위해서 loginview_asview제거하고 index함수가 로그인 과정 처리하도록 수정해서 해결했음.

--- ssulocker/locker/urls.py
+++ ssulocker/locker/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from . import views
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth import views as auth_views
 from .reserve import reserve_process as rsp
 app_name='locker'
 urlpatterns = [
     #path('', views.index, name='index'),
     path('lockerlist',views.lockerlist,name='lockerlist'),
-    path('login',auth_views.LoginView.as_view(template_name='locker/index.html'),name='login'),
+    path('login',views.index,name='login'),
     path('registeration_popup',views.registpop,name="rpop")
 ]
--- ssulocker/locker/views.py
+++ ssulocker/locker/views.py
@@ -1,16 +1,26 @@
 from django.contrib.auth import authenticate
-from django.shortcuts import render,get_object_or_404
-import django.contrib.auth as aut
+from django.shortcuts import render,get_object_or_404,redirect
+import django.contrib.auth as auth
 #from django.http import HttpResponse
 from .models import lockers
 import logging
 from django.db import connection
 def index(request):
     #학부 별 남은라커 수/라커 수 구해서 띄워줄 것임.
-    CS_left=lockers.objects.filter(department="CS",resreved=0).count()
-    CS_lockers=lockers.objects.filter(department="CS").count()
-    locker_context={"cs_left":CS_left,"cs_lockers":CS_lockers}
-    return render(request,'locker/index.html',locker_context)
+    cs_left=lockers.objects.filter(department="CS",reserved=0).count()
+    cs_lockers=lockers.objects.filter(department="CS").count()
+    locker_context={"cs_left":cs_left,"cs_lockers":cs_lockers}
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user=auth.authenticate(request,username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/locker/lockerlist')
+        else:
+            return render(request,'locker/index.html',{'error':'아이디 또는 패스워드가 일치하지 않습니다'})
+    else:
+        return render(request,'locker/index.html',locker_context)
 def lockerlist(request):
     locker_list=lockers.objects.order_by('lockernum')
     context={"locker_list":locker_list}
