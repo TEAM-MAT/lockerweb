2e58b0105ef249acdc9762f3e13d90b02e583af8
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-12-26 17:03:20 +0900

    Update settings.py

--- ssulocker/ssulocker/settings.py
+++ ssulocker/ssulocker/settings.py
@@ -132,6 +132,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'locker','static')
+STATICFILES_DIRS=[]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
