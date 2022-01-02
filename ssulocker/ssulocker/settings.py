674b844828701ea8667e45629e5dd15873dcc2bb
intersoom <sallybig@naver.com>
2022-01-03 01:31:26 +0900

    현재 선택한 사물함 추가, 예약 페이지 제작, 사물함 리스트 버튼 형식으로 수정

--- ssulocker/ssulocker/settings.py
+++ ssulocker/ssulocker/settings.py
@@ -131,7 +131,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'locker','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'locker','static')
 STATICFILES_DIRS=[]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
