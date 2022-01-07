03cf8b63d9bcac4d43c9a5c4be95ecf535aa37c0
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-01-07 20:24:15 +0900

    Update lockerlist.py

--- ssulocker/locker/views/lockerlist.py
+++ ssulocker/locker/views/lockerlist.py
@@ -5,18 +5,23 @@ from django.shortcuts import render,get_object_or_404,redirect
 import django.contrib.auth as auth
 from django.contrib.auth.decorators import login_required
 from ..models import lockers,users
+from .time_login import timecheck
 import logging
 import json
 from django.db import connection
 @login_required(login_url='locker:login')
 def lockerlist(request):
     if request.user.is_authenticated:
-        user=users.objects.get(id=request.user.id)
-        locker_list=lockers.objects.filter(department=user.department,reserved=0).order_by("lockernum")   
-        userlocker="예약된 사물함 없음"
-        if user.lockernum is not None:
-            userlocker=user.lockernum.lockernum
-        context={"locker_list":locker_list,"department":user.department,"username":user.name,"usercurrlocker":userlocker}
-        return render(request,'locker/lockerlist.html',context)
+        if timecheck(request.user.department)==1:
+            user=users.objects.get(id=request.user.id)
+            locker_list=lockers.objects.filter(department=user.department,reserved=0).order_by("lockernum")   
+            userlocker="예약된 사물함 없음"
+            if user.lockernum is not None:
+                userlocker=user.lockernum.lockernum
+            context={"locker_list":locker_list,"department":user.department,"username":user.name,"usercurrlocker":userlocker}
+            return render(request,'locker/lockerlist.html',context)
+        else:
+            auth.logout(request)
+            return redirect('/locker/login')
     else:
         return redirect('/locker/login')
