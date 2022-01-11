e052d203be4b0a1de5cf32193743f8081a0a2ccc
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-01-11 23:38:39 +0900

    pwchange form

--- ssulocker/locker/views/pwchange.py
+++ ssulocker/locker/views/pwchange.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from models import users
 @login_required(login_url='locker:login')
 def pwchange(request):
     if request.method=="POST":
-        
+        #form data받아서 처리
+    elif request.method=="GET":
+        #form 전송
+
+def cleaned_password()
