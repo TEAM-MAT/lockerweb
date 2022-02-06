f5f6cbc733e424b3ffddf216ff5b760bb92a0e47
SamSim <jun78111209@gmail.com>
2022-02-06 16:38:27 +0900

    reservation finish time

--- ssulocker/locker/views/time_login.py
+++ ssulocker/locker/views/time_login.py
@@ -2,7 +2,7 @@ from ..models import users,department
 import datetime
 def timecheck(usrdept):
     if datetime.datetime.now()>=department.getdepttime(usrdept):#예약시간 이후면
-        if department.getdepttime(usrdept)+datetime.timedelta(days=1)-datetime.timedelta(hours=2)>datetime.datetime.now(): #예약시간으로부터 22시간 까지 예약가능.
+        if department.getdeptfintime(usrdept)>datetime.datetime.now(): #예약시간으로부터 22시간 까지 예약가능.
             token=1
         else:
             token=0
